5c7d089c21ef29494525be087db2ff8e39ff0265
t3hmun <git@t3hmun.com>
2023-02-05 18:25:24 +0000

    Fix base colours.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,18 @@ const colors = require("tailwindcss/defaultTheme");
 module.exports = {
     darkMode: "class",
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {},
+    theme: {
+        extend: {
+            colors: {
+                main: colors.amber,
+                stone: {
+                    50: "#F8F8F7",
+                    150: "#EEEDEC",
+                    850: "#231F1E",
+                    950: "#0E0D0C",
+                },
+            },
+        },
+    },
     plugins: [],
 };

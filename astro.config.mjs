83ce099c1d3ba734cf052066b89fd72385dff5c4
t3hmun <git@t3hmun.com>
2023-02-21 19:22:02 +0000

    Add mdx, text page and use front matter in Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), preact()],
+    integrations: [tailwind(), preact(), mdx()],
 });

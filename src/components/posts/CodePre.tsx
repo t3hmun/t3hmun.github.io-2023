9a7f0ac6700eff71899a3e5a9e15d2f859c19400
t3hmun <git@t3hmun.com>
2023-03-05 22:56:11 +0000

    terrible design

--- src/components/posts/CodePre.tsx
+++ src/components/posts/CodePre.tsx
@@ -1,5 +1,5 @@
-import type { ComponentChildren } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import type { ComponentChildren, Ref } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
 import type { JSX } from "preact/jsx-runtime";
 
 type CodePreProps = {
@@ -16,10 +16,33 @@ export function CodePre(props: CodePreProps): JSX.Element {
     const title = props["data-title"];
     const [clientSideJs, setClientSideJs] = useState(false);
     const [expanded, setExpanded] = useState(false);
+    const [glowTooltip, setGlowTooltip] = useState(false);
+    const [copyClicked, setCopyClicked] = useState(0);
+    const [toolTip, setToolTip] = useState("default");
+    let codeEle = useRef<null | HTMLPreElement>(null);
     useEffect(() => {
         // This effect only runs client side, so it enables the features for the js enabled.
         setClientSideJs(true);
     });
+    useEffect(() => {
+        if (copyClicked == 0) return;
+        setGlowTooltip(true);
+        var timeout = setTimeout(() => {
+            setGlowTooltip(false);
+        }, 1000);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [copyClicked]);
+
+    function clip() {
+        if (!codeEle) return;
+        const innerText = codeEle.current?.innerText;
+        if (!innerText) return;
+        navigator.clipboard.writeText(innerText);
+        setCopyClicked((x) => x + 1);
+    }
+
     return (
         // Maybe max-wit-fit should be the default for code, and I don't need any code or interactivity...
         <div
@@ -30,10 +53,29 @@ export function CodePre(props: CodePreProps): JSX.Element {
             <div class="text-s flex justify-between rounded-t-md border-0 bg-stone-300 px-2 py-1 dark:bg-stone-700">
                 <div>{title}</div>
                 {clientSideJs && (
-                    <div class="">
+                    <div class="flex flex-row">
                         <div
-                            class="cursor-pointer rounded-r-full pr-0.5 pl-0.5 text-amber-500 hover:text-amber-400"
+                            class={`pr-0.5 pl-0.5 text-amber-400  transition ${
+                                glowTooltip
+                                    ? "text-amber-400"
+                                    : "text-amber-100"
+                            }`}
+                        >
+                            {toolTip}
+                        </div>
+                        <div
+                            class="cursor-pointer pr-2 pl-0.5 text-amber-500 hover:text-amber-400"
+                            onClick={clip}
+                            onMouseEnter={() => setToolTip("Copy")}
+                            onMouseLeave={() => setToolTip("")}
+                        >
+                            📋
+                        </div>
+                        <div
+                            class="cursor-pointer pr-0.5 pl-0.5 text-amber-500 hover:text-amber-400"
                             onClick={() => setExpanded((s) => !s)}
+                            onMouseEnter={() => setToolTip("Expand")}
+                            onMouseLeave={() => setToolTip("")}
                         >
                             ↔
                         </div>
@@ -41,6 +83,7 @@ export function CodePre(props: CodePreProps): JSX.Element {
                 )}
             </div>
             <pre
+                ref={codeEle}
                 {...props}
                 class={`mx-2 overflow-x-auto px-2 py-1 font-mono ${props.class} `}
             >

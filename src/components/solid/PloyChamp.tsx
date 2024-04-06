e1e089e647f3b72cf05932af92c1c9209d139fd9
t3hmun <git@t3hmun.com>
2024-04-06 18:07:42 +0100

    Kinda works but but the single game func is terible

--- src/components/solid/PloyChamp.tsx
+++ src/components/solid/PloyChamp.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource solid-js */
 
-import { createEffect, createSignal, type JSX } from "solid-js";
+import { createEffect, createSignal, For, type JSX } from "solid-js";
 import { useKeyDownEvent } from "@solid-primitives/keyboard";
 
 type Direction = "u" | "d" | "l" | "r";
@@ -15,11 +15,17 @@ const stratagems: Record<StratagemName, Stratagem> = {
     Resupply: { name: "Resupply", directions: ["d", "d", "u", "r"] },
 };
 
-type KeyAttempt = {
-    expected: Direction;
-    actual: Direction;
-    success: boolean;
-};
+type KeyAttempt =
+    | {
+          expected: Direction;
+          actual: Direction;
+          success: boolean;
+      }
+    | {
+          expected: Direction;
+          actual: undefined;
+          success: undefined;
+      };
 
 type CompletedAttempt = {
     stratagem: Stratagem;
@@ -35,8 +41,9 @@ type GameState = {
 };
 
 function printAttempt(attempt: Array<KeyAttempt>) {
-    return attempt.map((a) => `[${a.actual},${a.expected}]`).join(" ");
+    return attempt.map((a) => `[${a.actual ?? ""},${a.expected}]`).join(" ");
 }
+
 function printCompleted(completed: Array<CompletedAttempt>) {
     return completed
         .map(
@@ -45,8 +52,74 @@ function printCompleted(completed: Array<CompletedAttempt>) {
         )
         .join("\n");
 }
+
 function printState(state: GameState) {
     return `${state.stratagem.name} ${state.keySequence.join(" ")}\n${printAttempt(state.attempt)}\n${printCompleted(state.completed)}`;
+}
+
+function GameStateLogic(prev: GameState, direction: Direction) {
+    let seq = [...prev.keySequence, direction];
+    const stratagem = prev.stratagem;
+
+    // Just re-evaluate the whole attempt, it is cheap, simple and works when you reset the key sequence.
+    let attempt: Array<KeyAttempt> = [];
+
+    stratagem.directions.forEach((expected, i) => {
+        const actual = seq[i];
+        if (actual === undefined) {
+            attempt.push({
+                expected,
+                actual: undefined,
+                success: undefined,
+            });
+            return;
+        }
+        const success = expected === actual;
+        attempt.push({
+            expected,
+            actual,
+            success: success,
+        });
+    });
+
+    const completedAttempts = [...prev.completed];
+
+    const success = attempt.every((a) => a.success === true);
+    const complete = success || attempt.some((a) => a.success === false);
+
+    if (complete) {
+        completedAttempts.unshift({
+            stratagem: stratagem,
+            success: success,
+            attempt: attempt,
+        });
+        attempt = stratagem.directions.map((d) => ({
+            expected: d,
+            actual: undefined,
+            success: undefined,
+        }));
+        seq = [];
+    }
+
+    return {
+        stratagem: prev.stratagem,
+        keySequence: seq,
+        attempt: attempt,
+        completed: completedAttempts,
+    };
+}
+
+function initState(stratagem: Stratagem): GameState {
+    return {
+        stratagem: stratagem,
+        keySequence: [],
+        attempt: stratagem.directions.map((d) => ({
+            expected: d,
+            actual: undefined,
+            success: undefined,
+        })),
+        completed: [],
+    };
 }
 
 export function PloyChamp() {
@@ -62,12 +135,9 @@ export function PloyChamp() {
         arrowright: "r",
     };
 
-    const [gameState, setGameState] = createSignal<GameState>({
-        stratagem: stratagems.Resupply,
-        keySequence: [],
-        attempt: [],
-        completed: [],
-    });
+    const [gameState, setGameState] = createSignal<GameState>(
+        initState(stratagems.Resupply),
+    );
 
     const event = useKeyDownEvent();
 
@@ -78,69 +148,45 @@ export function PloyChamp() {
             const key = e.key.toLowerCase(); // The a mapping for keys with word names like ArrowUp will break if toLowerCase is removed.
             const direction = m[key];
             if (direction) {
-                setGameState((prev) => {
-                    let seq = [...prev.keySequence, direction];
-
-                    // Just re-evaluate the whole attempt, it is cheap, simple and works when you reset the key sequence.
-                    let attempt: Array<KeyAttempt> = [];
-                    for (let i = 0; i < seq.length; i++) {
-                        const actual = seq[i]!;
-                        if (i >= prev.stratagem.directions.length) {
-                            // Once the strat is finished extra keys don't matter.
-                            break;
-                        }
-                        const expected = prev.stratagem.directions[i]!;
-                        const success = expected === actual;
-                        attempt.push({
-                            expected,
-                            actual,
-                            success: success,
-                        });
-                    }
-
-                    const completedAttempts = [...prev.completed];
-
-                    if (!attempt.every((a) => a.success)) {
-                        completedAttempts.push({
-                            stratagem: prev.stratagem,
-                            success: false,
-                            attempt: attempt,
-                        });
-                        attempt = [];
-                        seq = [];
-                    } else if (
-                        attempt.length === prev.stratagem.directions.length
-                    ) {
-                        completedAttempts.push({
-                            stratagem: prev.stratagem,
-                            success: true,
-                            attempt: attempt,
-                        });
-                        attempt = [];
-                        seq = [];
-                    }
-                    return {
-                        stratagem: prev.stratagem,
-                        keySequence: seq,
-                        attempt: attempt,
-                        completed: completedAttempts,
-                    };
-                });
+                setGameState((prev) => GameStateLogic(prev, direction));
             }
         }
     });
 
     return (
         <>
-            <h1>yo</h1>
             <Button
                 class="my-2"
-                onClick={() => setGameState((p) => ({ ...p, keySequence: [] }))}
+                onClick={() => setGameState((p) => initState(p.stratagem))}
             >
-                Clear
+                Reset
             </Button>
-            <p class="font-mono">KEYS:</p>
-            <pre class="whitespace-pre">{printState(gameState())}</pre>
+
+            <div class="flex justify-center">
+                <div class="w-96">
+                    <div class="bg-yellow-500 text-slate-900 text-center">
+                        {gameState().stratagem.name.toUpperCase()}
+                    </div>
+                    <div class="text-center">
+                        <For each={gameState().attempt}>
+                            {(a) => <ActiveKeyAttempt keyAttempt={a} />}
+                        </For>{" "}
+                    </div>
+                    <div class="text-center d-flex flex-column">
+                        <For each={gameState().completed}>
+                            {(c) => (
+                                <div>
+                                    <For each={c.attempt}>
+                                        {(a) => (
+                                            <ActiveKeyAttempt keyAttempt={a} />
+                                        )}
+                                    </For>
+                                </div>
+                            )}
+                        </For>
+                    </div>
+                </div>
+            </div>
 
             <h2 class="text-2xl mt-5 mb-3">Issues</h2>
             <ul class="list-disc pl-5">
@@ -157,6 +203,9 @@ export function PloyChamp() {
                     possible strat and fails you immediately, keeps it simple.
                 </li>
             </ul>
+
+            <h2 class="text-2xl mt-5 mb-3">Game State Debug</h2>
+            <pre class="whitespace-pre">{printState(gameState())}</pre>
         </>
     );
 }
@@ -175,5 +224,36 @@ function Button(props: ButtonProps) {
         >
             {props.children}
         </button>
+    );
+}
+
+type ActiveKeyAttemptProps = {
+    keyAttempt: KeyAttempt;
+};
+
+const CharMap: Record<Direction, string> = {
+    u: "⇧",
+    d: "⇩",
+    l: "⇦",
+    r: "⇨",
+};
+
+function ActiveKeyAttempt(props: ActiveKeyAttemptProps): JSX.Element {
+    let color: string;
+    switch (props.keyAttempt.success) {
+        case true:
+            color = "text-green-500";
+            break;
+        case false:
+            color = "text-red-500";
+            break;
+        default:
+            color = "text-gray-500";
+    }
+
+    return (
+        <span class={`text-4xl text-center mx-3 ${color} `}>
+            {CharMap[props.keyAttempt.expected]}
+        </span>
     );
 }

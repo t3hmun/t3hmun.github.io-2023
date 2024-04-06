dca91a9026e8333edd63443e6cabfec038ece1b1
t3hmun <git@t3hmun.com>
2024-04-06 13:01:36 +0100

    Key events work

--- src/components/solid/PloyChamp.tsx
+++ src/components/solid/PloyChamp.tsx
@@ -1,80 +1,92 @@
 /** @jsxImportSource solid-js */
 
-import { For, createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
+import { useKeyDownEvent } from "@solid-primitives/keyboard";
+
+type Direction = "u" | "d" | "l" | "r";
+type StratagemName = "Resupply";
+
+type Stratagem = {
+    name: StratagemName;
+    directions: Array<Direction>;
+};
+
+const stratagems: Record<StratagemName, Stratagem> = {
+    Resupply: { name: "Resupply", directions: ["d", "d", "u", "r"] },
+};
 
 export function PloyChamp() {
-    const [thing, setThing] = createSignal("inital");
-    const [things, setThings] = createSignal<Array<string>>([]);
-    const [challenges, setChallenges] = createSignal<Array<ChallengeProps>>([]);
+    /** If I decide to make this editable then need to add another "no-in-game" signal to suspend accepting new key-presses? */
+    const mapping: Record<string, Direction> = {
+        w: "u",
+        s: "d",
+        a: "l",
+        d: "r",
+        arrowup: "u",
+        arrowdown: "d",
+        arrowleft: "l",
+        arrowright: "r",
+    };
+
+    const [lastSeq, setLastSeq] = createSignal<Array<Direction>>([]);
+    const [stratagem, setStratagem] = createSignal<Stratagem>(
+        stratagems.Resupply,
+    );
+
+    const event = useKeyDownEvent();
+
+    createEffect(() => {
+        const e = event();
+        const m = mapping;
+        if (e && !e.repeat) {
+            const key = e.key.toLowerCase(); // The a mapping for keys with word names like ArrowUp will break if toLowerCase is removed.
+            const direction = m[key];
+            if (direction) {
+                setLastSeq((prev) => [...prev, direction]);
+            }
+        }
+    });
 
     return (
         <>
-            <p>{thing()}</p>
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-                onClick={() => setThing("A") && setThings([...things(), "A"])}
-            >
-                A
-            </button>
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => setThing("B") && setThings([...things(), "B"])}
-            >
-                B
-            </button>
-            <Challenge stratagem={["A", "B"]} attempt={things()} />
+            <h1>yo</h1>
+            <Button class="my-2" onClick={() => setLastSeq([])}>
+                Clear
+            </Button>
+            <p class="font-mono">KEYS: {lastSeq()}</p>
+
+            <h2 class="text-2xl mt-5 mb-3">Issues</h2>
+            <ul class="list-disc pl-5">
+                <li>
+                    Holding down keys occasionally double registers, but you
+                    shouldn't be holding down keys anyway.
+                </li>
+                <li>
+                    Trying to capture the ctrl key in the browser is dodgy, so
+                    this game simply ignores modifier keys.
+                </li>
+                <li>
+                    This game pretends the current strat is the only only
+                    possible strat and fails you immediately, keeps it simple.
+                </li>
+            </ul>
         </>
     );
 }
 
 type ButtonProps = {
     onClick: () => void;
-    children:
-}
-
-function Button(){}
-
-type ChallengeProps = {
-    stratagem: Array<string>;
-    attempts: Array<Array<PressProps>>;
+    children: JSX.Element;
+    class?: string;
 };
 
-function Challenge(props: ChallengeProps) {
+function Button(props: ButtonProps) {
     return (
-        <>
-            <p class="text-center">
-                <For each={props.stratagem}>
-                    {(thing) => <span class="mr-2 text-blue-500">{thing}</span>}
-                </For>
-            </p>
-            <ul>
-                <For each={props.attempts}>
-                    {(attempt) => (
-                        <li>
-                            <ul class="flex flex-wrap items-center justify-center">
-                                <For each={attempt}>
-                                    {(pressProps) => (
-                                        <li>
-                                            <Press {...pressProps} />
-                                        </li>
-                                    )}
-                                </For>
-                            </ul>
-                        </li>
-                    )}
-                </For>
-            </ul>
-        </>
+        <button
+            class={`bg-orange-800 rounded active:scale-95  focus:shadow py-1 px-2 ${props.class}`}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
     );
-}
-
-type PressProps = {
-    direction: string;
-    valid: boolean;
-};
-
-function Press(props: PressProps) {
-    // TODO: Figure out if Solid really wants me to use a function here
-    const color = () => props.valid ? "text-green-500" : "text-red-500";
-    return <span class={`${color()}`}>{props.direction}</span>;
 }

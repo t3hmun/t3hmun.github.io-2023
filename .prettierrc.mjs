9853293d64d21ac93dedc8b64570d2198733de1f
t3hmun <git@t3hmun.com>
2025-01-05 18:10:22 +0000

    new formatter

--- .prettierrc.mjs
+++
@@ -1,6 +0,0 @@
-// .prettierrc.mjs
-// see: https://github.com/withastro/prettier-plugin-astro
-/** @type {import("prettier").Config} */
-export default {
-    plugins: ["prettier-plugin-astro"],
-};
